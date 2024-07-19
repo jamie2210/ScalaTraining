089ff1942aab36ae7261c04e1f5722445378f2d9
Jamie Letts <jamieletts2210@gmail.com>
2024-07-19 16:54:35 +0100

    add testing for each method and outcome

--- src/main/scala/Week3/Friday/VinnieAfternoonTask.scala
+++ src/main/scala/Week3/Friday/VinnieAfternoonTask.scala
@@ -2,35 +2,18 @@ package Week3.Friday
 
 object VinnieAfternoonTask extends App {
 
-  // create two case classes 'Name' and Postcode - value string
   case class Name(value: String)
   case class Postcode(value: String)
 
-  // Create a case class called Letter' that contains a name' and postcode
-  //  - The 'name' should have the type 'Name'
-  //  - The postcode should have the type
   case class Letter(name: Name, postcode: Postcode)
 
-  //  Create a trait called 'GenericPostageError'
-  //  - Give it a parameter method 'message' with a type "String'
   trait GenericPostageError {
     def message: String
   }
-  //  Create a case class called 'Invalid NameError'
-  //  - Give it a parameter message with a type String'
-  //  - Let it be extended by 'GenericPostageError'
+
   case class InvalidNameError(message: String) extends GenericPostageError
 
-  //  Create a case class called InvalidPostcodeError
-  //  - Give it a parameter message with a type "String'
-  //  - Let it be extended by GenericPostageError
   case class InvalidPostcodeError(message: String) extends GenericPostageError
-
-  //  Create the following variables that have a type
-  //  'Either[GenericPostageError, Letter]'
-  //  - val letter : A letter constructed using the 'apply' method of 'Letter'
-  //  - 'val invalidLetter': An error constructed using the '.apply' method of
-  //  "InvalidNameError first
 
   val letter: Right[Nothing, Letter] =
     Right(Letter(name = Name("Jon"), postcode = Postcode("SW17 9TA")))
@@ -38,23 +21,17 @@ object VinnieAfternoonTask extends App {
   val invalidLetter: Left[InvalidNameError, Nothing] =
     Left(InvalidNameError("Invalid name provided"))
 
-  // Print the values to see if they are 'Right' or 'Left'
-  println(s"letter: $letter")
-  println(s"invalidLetter: $invalidLetter")
-
   object Postcode {
     def postcodeOrError(maybePostCode: String): Either[InvalidPostcodeError, Postcode] = {
       val postcodePattern = """^\S+\s+\S+$""".r
-      if (postcodePattern.matches(maybePostCode)) Right(Postcode(maybePostCode)) else
-        Left(InvalidPostcodeError(s"Invalid postcode: $maybePostCode"))
+      if (postcodePattern.matches(maybePostCode)) Right(Postcode(maybePostCode)) else Left(InvalidPostcodeError(s"Invalid postcode: $maybePostCode"))
     }
   }
 
   object Name {
-    def nameOrError(mayBeName: String): Either[InvalidNameError, Name] = {
-      if (mayBeName.exists(_.isDigit)) Left (InvalidNameError(mayBeName)) else
-        Right(Name(mayBeName))
-
+    def nameOrError(maybeName: String): Either[InvalidNameError, Name] = {
+      if (maybeName.exists(_.isDigit)) Left(InvalidNameError(s"Invalid name: $maybeName")) else
+        Right(Name(maybeName))
     }
   }
 
@@ -66,18 +43,4 @@ object VinnieAfternoonTask extends App {
     } yield Letter(name, postcode)
     }
   }
-
-  val letterWithANumberInName: Either[GenericPostageError, Letter] =
-    Letter.letterOrError("J4mie", "sw17 9TA")
-
-  val letterWithAnInvalidPostcode: Either[GenericPostageError, Letter] =
-    Letter.letterOrError("Jamie", "SW179TA")
-
-  val letterAllInvalid: Either[GenericPostageError, Letter] =
-    Letter.letterOrError("Jamie", "SW17 9TA")
-
-  // Print the results
-  println(s"letterWithANumberInName: $letterWithANumberInName")
-  println(s"letterWithAnInvalidPostcode: $letterWithAnInvalidPostcode")
-  println(s"letterAllInvalid: $letterAllInvalid")
 }
--- src/test/scala/Week3/VinnieAfternoonTaskSpec.scala
+++ src/test/scala/Week3/VinnieAfternoonTaskSpec.scala
@@ -1,8 +1,69 @@
 package Week3
 
+import Week3.Friday.VinnieAfternoonTask._
+//import Week3.Friday.VinnieAfternoonTask.{InvalidNameError, Letter, Name, Postcode}
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers
 
 class VinnieAfternoonTaskSpec extends AnyWordSpec with Matchers {
 
+    "Name" should {
+      "create a letter" in {
+        val letter: Right[Nothing, Letter] =
+          Right(Letter(name = Name("Jon"), postcode = Postcode("SW17 9TA")))
+
+        letter shouldBe (Right(Letter(Name("Jon"),Postcode("SW17 9TA"))))
+      }
+      "return an invalid error" in {
+        val invalidLetter: Left[InvalidNameError, Nothing] =
+          Left(InvalidNameError("Invalid name provided"))
+
+        invalidLetter shouldBe Left(InvalidNameError("Invalid name provided"))
+      }
+    }
+
+  "Postcode" should {
+    "return a valid postcode" in {
+      val result = Postcode.postcodeOrError("SW17 0RZ")
+      result shouldBe (Right(Postcode("SW17 0RZ")))
+    }
+    "return error message" in {
+      val result = Postcode.postcodeOrError("SW170RZ")
+      result shouldBe (Left(InvalidPostcodeError("Invalid postcode: SW170RZ")))
+    }
+  }
+
+  "Name" should {
+    "return a valid name" in {
+      val result = Name.nameOrError("Jamie")
+      result shouldBe (Right(Name("Jamie")))
+    }
+  }
+  "return error message" in {
+    val result = Name.nameOrError("J4mie")
+    result shouldBe (Left("Invalid name: J4mie"))
+  }
+
+  "Letter" should {
+    "return a right" when {
+      "all inputs pass" in {
+        val inputs = Letter.letterOrError("Jamie", "SW17 9TA")
+        inputs shouldBe Right(Letter(Name("Jamie"),Postcode("SW17 9TA")))
+      }
+      "return a left" when {
+        "name input is invalid" in {
+          val inputs = Letter.letterOrError("J4mie", "SW17 9TA")
+          inputs shouldBe Left(InvalidNameError("Invalid name: J4mie"))
+        }
+        "postcode input is invalid" in {
+          val input = Letter.letterOrError("Jamie", "SW179TA")
+          input shouldBe Left(InvalidPostcodeError("Invalid postcode: SW179TA"))
+        }
+        "either name or postcode are invalid" in {
+          val input = Letter.letterOrError("J4mie", "SW179TA")
+          input shouldBe Left(InvalidNameError("Invalid name: J4mie"))
+        }
+      }
+    }
+  }
 }
